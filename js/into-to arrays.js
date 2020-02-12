dcc08ee5015c7d7fe9bf3b599bd409c71df78a36
James Spicer <spicer.r.james@gmail.com>
2020-02-12 16:32:08 -0600

    arrays execises

---
+++ js/into-to arrays.js
@@ -0,0 +1,58 @@
+"use strict";
+/* ***************************************************************
+* 					ITERATING ARRAYS
+*************************************************************** */
+//  TODO TOGETHER: Log each element of the shapes array
+// var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+//
+// for(var i = 0; i < shapes.length; i++){
+//     console.log(shapes[i]);
+
+
+// TODO TOGETHER: Using a for loop, iterate through the shapes array and log each shape
+// var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+//
+// for(var i = 0; i < shapes.length; i++){
+//     console.log("The shape at index " + i + " is " + shapes[i]);
+// }
+
+// TODO: What happens if we change var i = 1? or var i = 2;
+// TODO: What are benefits of using loops to iterate?
+// TODO: How does the loop know when to stop iterating?
+
+// TODO: Using a for loop, iterate through the instructors array and console.log each instructor
+// var instructors = ["sophie", "vivian", "david", "justin", "daniel", "fernando", "trant"];
+// for(var i = 0; i < instructors.length; i++){
+//     console.log(instructors[i]);
+// }
+
+// TODO: Using a for loop, iterate through the daysOfTheWeek array and console.log each day of the week
+// var daysOfTheWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+// for(var i = 0; i < daysOfTheWeek.length; i++){
+//     console.log(daysOfTheWeek[i]);
+// }
+
+
+// TODO: Using a for loop, iterate through the favoriteFoods array and console.log each favorite food
+// var favoritesFoods = ["Chinese", "Sushi", "Pizza", "Borger"];
+// for(var i = 0; i < favoritesFoods.length; i++){
+//     console.log(favoritesFoods[i]);
+// }
+
+// TODO: Refactoring the instructor loop, alert "hey, I know <INSTRUCTOR NAME HERE>" if you have had class with that instructor. If you have not had class with that instructor, alert "I haven't had class with <INSTRUCTOR NAME HERE> yet!"
+var instructors = ["sophie", "vivian", "david", "justin", "daniel", "fernando", "trant"];
+for(var i = 0; i < instructors.length; i++){
+    console.log(instructors[i]);
+}if(instructors[i] === "david"){
+    console.log("Hey! I know" + i);
+}
+
+
+/* ***************************************************************
+* 						FOR EACH ARRAY
+*************************************************************** */
+
+// TODO: Using a for each loop, iterate through the instructors array and console.log each instructor
+// TODO: Using a for each loop, iterate through the daysOfTheWeek array and console.log each day of the week
+// TODO: Using a for each loop, iterate through the favoriteFoods array and console.log each favorite food
+
