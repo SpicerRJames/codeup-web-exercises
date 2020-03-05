2d9b5eb04301dbb66c079905c3275aa09078a96f
James Spicer <spicer.r.james@gmail.com>
2020-03-05 14:53:52 -0600

    weather map api project, added the map and some functionality

--- js/keys.js
+++ js/keys.js
@@ -1,1 +1,3 @@
 var mapboxToken = "pk.eyJ1Ijoic3BpY2VyLXItamFtZXMiLCJhIjoiY2s2dHR0ZXFsMDBxZDNlcDlnc3plbzVqMyJ9.FnpGCvcDDEitt_0D_d6Pfw";
+
+var darkSkyKey = "aec58531fe9e053e65d6fed3cd8f04e4";
---
+++ js/weathermap.js
@@ -0,0 +1,29 @@
+var weatherObjects = [
+    {
+        currentWeather: "clear-day",
+    },
+    {
+        currentWeather: "clear-night",
+    },
+    {
+        currentWeather: "rain",
+    },
+    {
+        currentWeather: "snow",
+    },
+    {
+        currentWeather: "wind",
+    },
+    {
+        currentWeather: "fog",
+    },
+    {
+        currentWeather: "cloudy",
+    },
+    {
+        currentWeather: "partly-cloudy-day",
+    },
+    {
+        currentWeather: "partly-cloudy-night",
+    }
+];
